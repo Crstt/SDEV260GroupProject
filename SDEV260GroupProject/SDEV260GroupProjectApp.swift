c3043e99e3554701e74b2db27e8cd2d0c856bd76
Crstt <matteo598.catalano@gmail.com>
2023-11-10 15:09:08 -0500

    Initial Commit

---
+++ SDEV260GroupProject/SDEV260GroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDEV260GroupProjectApp.swift
+//  SDEV260GroupProject
+//
+//  Created by Matteo Catalano on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SDEV260GroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
